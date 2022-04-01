fa104d6507f392707cacaef2a672c31352908444
Ana Motta <mottaanapaula98@gmail.com>
2022-04-01 10:12:08 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/example/cursoSpring/entities/Product.java
+++ src/main/java/com/example/cursoSpring/entities/Product.java
@@ -1,9 +1,7 @@
 package com.example.cursoSpring.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -12,17 +10,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 @Entity
-@Table(name="tb_product")
-public class Product implements Serializable{
-	
+@Table(name = "tb_product")
+public class Product implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -30,19 +27,19 @@ public class Product implements Serializable{
 	private String description;
 	private double price;
 	private String imgUrl;
-	
+
 	@ManyToMany
-	@JoinColumn(name = "id_category")
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
-	@JsonIgnore
-	@ManyToMany(mappedBy = "items")
-	private List<Order> orders = new ArrayList<>();
-	
+
+//	@JsonIgnore
+//	@ManyToMany(mappedBy = "items")
+//	private List<Order> orders = new ArrayList<>();
+
 	public Product() {
-		
+
 	}
-	
+
 	public Product(Long id, String name, String description, double price, String imgUrl) {
 		this.name = name;
 		this.description = description;
@@ -94,14 +91,14 @@ public class Product implements Serializable{
 		return categories;
 	}
 
-	public List<Order> getOrders() {
-		return orders;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(categories, description, id, imgUrl, name, orders, price);
-	}
+//	public List<Order> getOrders() {
+//		return orders;
+//	}
+//
+//	@Override
+//	public int hashCode() {
+//		return Objects.hash(categories, description, id, imgUrl, name, orders, price);
+//	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -114,10 +111,8 @@ public class Product implements Serializable{
 		Product other = (Product) obj;
 		return Objects.equals(categories, other.categories) && Objects.equals(description, other.description)
 				&& Objects.equals(id, other.id) && Objects.equals(imgUrl, other.imgUrl)
-				&& Objects.equals(name, other.name) && Objects.equals(orders, other.orders)
+//				&& Objects.equals(name, other.name) && Objects.equals(orders, other.orders)
 				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price);
 	}
 
-	
-	
 }
